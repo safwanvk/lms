98f338ece3c0e0e05b4c45f9ca96eefa8e98e871
safwanvk <safwanvalakundil@gmail.com>
2021-04-09 16:48:14 +0530

    :sparkles: Setup button connections

--- index.py
+++ index.py
@@ -10,6 +10,27 @@ class MainApp(QMainWindow , ui):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setupUi(self)
+        self.handle_buttons()
+        self.tabWidget.tabBar().setVisible(False)
+        
+    def handle_buttons(self):
+        self.pushButton.clicked.connect(self.day_to_day_tabs)
+        self.pushButton_2.clicked.connect(self.books_tabs)
+        self.pushButton_3.clicked.connect(self.users_tabs)
+        self.pushButton_4.clicked.connect(self.settings_tab)
+
+    #####Open Tabs######
+    def day_to_day_tabs(self):
+        self.tabWidget.setCurrentIndex(0)
+
+    def books_tabs(self):
+        self.tabWidget.setCurrentIndex(1)
+
+    def users_tabs(self):
+        self.tabWidget.setCurrentIndex(2)
+
+    def settings_tab(self):
+        self.tabWidget.setCurrentIndex(3)
 
 def main():
     app = QApplication(sys.argv)
