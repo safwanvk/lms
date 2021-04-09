ce1b6f3158bf510b448fa07ac80ec32ac5a94880
safwanvk <safwanvalakundil@gmail.com>
2021-04-09 21:58:47 +0530

    :sparkles: Setup db config

--- index.py
+++ index.py
@@ -1,8 +1,16 @@
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+import mysql.connector
 import sys
 from PyQt5.uic import loadUiType
+
+db = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="1234",
+  database="lms"
+)
 
 ui,_ = loadUiType('library.ui')
 
@@ -19,6 +27,8 @@ class MainApp(QMainWindow , ui):
         self.pushButton_3.clicked.connect(self.users_tabs)
         self.pushButton_4.clicked.connect(self.settings_tab)
 
+        self.pushButton_12.clicked.connect(self.add_new_book)
+
     #####Open Tabs######
     def day_to_day_tabs(self):
         self.tabWidget.setCurrentIndex(0)
@@ -31,6 +41,43 @@ class MainApp(QMainWindow , ui):
 
     def settings_tab(self):
         self.tabWidget.setCurrentIndex(3)
+
+    ######Day To Day#######
+
+    ######Books############
+    def add_new_book(self):
+        self.cur = db.cursor()
+
+        self.cur.execute(''' SELECT  * FROM books''')
+        data = self.cur.fetchall()
+
+        print(data)
+
+    def edit_book(self):
+        pass
+
+    def delete_book(self):
+        pass
+
+    ######Users##########
+    def add_new_user(self):
+        pass
+
+    def edit_user(self):
+        pass
+
+    def user_login(self):
+        pass
+
+    #####settings#######
+    def add_new_category(self):
+        pass
+
+    def add_new_author(self):
+        pass
+
+    def add_new_publisher(self):
+        pass
 
 def main():
     app = QApplication(sys.argv)
